71b7f45679ef941d9eaaaacb9f4bba76ccb4f3d9
Naphong Chadha <harrajc@gmail.com>
2025-05-25 14:27:08 +0700

    create skeleton signup page and navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,19 @@
+import { cn } from "~/lib/utils";
+
+export function NavBar({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"nav">) {
+  return (
+    <nav
+      className={
+        cn("flex w-full items-center justify-between border-b p-4 text-xl font-semibold", className)
+      }
+      {...props}
+    >
+      <div>TikTok</div>
+      {/* TODO: Update to link to sign up page */}
+      <div>Sign Up</div>
+    </nav>
+  );
+}
