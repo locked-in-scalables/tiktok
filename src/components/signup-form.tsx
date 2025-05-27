c666f8a9709cbe4e2363c3bcf02c01beaec5d169
Naphong Chadha <harrajc@gmail.com>
2025-05-27 08:22:48 +0700

    finished signup and login page

--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -14,19 +14,59 @@ interface SignupFormProps extends React.ComponentPropsWithoutRef<"div"> {
   isLogInPage?: boolean;
 }
 
+interface FormDetails {
+  cardTitle: string;
+  cardDescription: string;
+  buttonText: string;
+  footerText: React.ReactNode;
+}
+
+function getFormDetails(isLogInPage: boolean): FormDetails {
+  if (isLogInPage) {
+    return {
+      cardTitle: "Log in to TikTok",
+      cardDescription: "Enter your email below to login to your account",
+      buttonText: "Login",
+      footerText: (
+        <div className="text-center text-sm">
+          Don&apos;t have an account?{" "}
+          <a href="/signup" className="underline underline-offset-4">
+            Sign up
+          </a>
+        </div>
+      ),
+    };
+  }
+
+  return {
+    cardTitle: "Sign up for TikTok",
+    cardDescription: "Enter your email below to create your account",
+    buttonText: "Sign up",
+    footerText: (
+      <div className="text-center text-sm">
+        Already have an account?{" "}
+        <a href="/login" className="underline underline-offset-4">
+          Log in
+        </a>
+      </div>
+    ),
+  };
+}
+
 export function SignupForm({
   isLogInPage = false,
   className,
   ...props
 }: SignupFormProps) {
+  const { cardTitle, cardDescription, buttonText, footerText } =
+    getFormDetails(isLogInPage);
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader className="text-center">
-          <CardTitle className="text-xl">Create an account</CardTitle>
-          <CardDescription>
-            Enter your email below to create an account
-          </CardDescription>
+          <CardTitle className="text-xl">{cardTitle}</CardTitle>
+          <CardDescription>{cardDescription}</CardDescription>
         </CardHeader>
         <CardContent>
           <form>
@@ -56,23 +96,10 @@ export function SignupForm({
                   <Input id="password" type="password" required />
                 </div>
                 <Button type="submit" className="w-full">
-                  Sign up
+                  {buttonText}
                 </Button>
               </div>
-              {isLogInPage && (
-                <div className="text-center text-sm">
-                  Don&apos;t have an account?{" "}
-                  <a href="#" className="underline underline-offset-4">
-                    Sign up
-                  </a>
-                </div>
-              )}
-              <div className="text-center text-sm">
-                Already have an account?{" "}
-                <a href="/login" className="underline underline-offset-4">
-                  Log in
-                </a>
-              </div>
+              {footerText}
             </div>
           </form>
         </CardContent>
